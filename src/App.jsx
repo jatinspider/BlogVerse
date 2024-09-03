f39d7dcb6bec6af2c1f42cfe2bd40007ea63b1a5
jatinspider <dbfanclub13@gmail.com>
2024-09-04 00:14:31 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
      },[dispatch]);
 
   return !loading ?(
-    <div className='min-h-screen flex flex-wrap content-between bg-pink-400'>
+    <div className='min-h-screen flex flex-wrap content-between bg-blue-600'>
       <div className='w-full block'>
         <Header/>
         <main>
